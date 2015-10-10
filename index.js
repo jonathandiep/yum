e2957b2a6c17d28fac8ad6b967b0fe063e260a8d
Jonathan Diep <jonathandiep@hotmail.com>
2015-10-10 14:17:13 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var path = require('path');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+  next();
+});
+
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
+
+var yelp = require("yelp").createClient({
+  consumer_key: "pcvmB-XbNmyiuu1YaQ0wsw",
+  consumer_secret: "s-_nYxipYIC-61Yzj6Fe8FvpuX0",
+  token: "X_7aMxEqS18PBOiNrXa2f-qxuGfI4ZJf",
+  token_secret: "xVc1FGsqLuOZw-BHPurnQtuAYv8"
+});
+
+app.get('/', function(req, res) {
+  res.sendFile('/public/index.html');
+})
+
+app.get('/ll/:ll', function(req, res) {
+  var ll = req.params.ll;
+  console.log(ll);
+  yelp.search({term: "food", ll: ll}, function(err, data) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else {
+      console.log(data);
+      res.send(data);
+    }
+  });
+})
+
+app.listen(3000);
+console.log('working');
---
+++ public/app/app.js
@@ -0,0 +1,44 @@
+var app = angular.module('app', []);
+
+app.controller('mainController', ['$scope', '$http', function($scope, $http) {
+  var ll;
+  var numValue = 0;
+  var yelpData = [];
+  $scope.matches = [];
+  $scope.processing = true;
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    ll = position.coords.latitude + ',' + position.coords.longitude;
+    console.log(ll);
+    $http.get('http://localhost:3000/ll/' + ll)
+      .then(function(data) {
+        $scope.processing = false;
+        console.log(data.data);
+        console.log(data.data.businesses);
+        yelpData = data.data;
+        $scope.picture = (yelpData.businesses[numValue].image_url).substr(0, yelpData.businesses[numValue].image_url.length - 6) + 'ls.jpg';
+        $scope.name = yelpData.businesses[numValue].name;
+        $scope.distance = yelpData.businesses[numValue].distance;
+      });
+  });
+
+  $scope.dislike = function() {
+    if (numValue >= 19) {
+      $scope.hideStuff = true;
+      $scope.showMatches = true;
+    } else {
+      numValue++;
+      console.log(numValue + ': ' + $scope.name);
+      $scope.picture = (yelpData.businesses[numValue].image_url).substr(0, yelpData.businesses[numValue].image_url.length - 6) + 'ls.jpg';
+      $scope.name = yelpData.businesses[numValue].name;
+      $scope.distance = yelpData.businesses[numValue].distance;
+    }
+  };
+
+  $scope.like = function() {
+    console.log(yelpData.businesses[numValue].name);
+    $scope.matches.push(yelpData.businesses[numValue]);
+    $scope.dislike();
+  }
+
+}]);
