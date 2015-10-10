d869173538b3c45f7ff9870c8675ab9adb6ea6e4
Jonathan Diep <jonathandiep@hotmail.com>
2015-10-10 15:54:17 -0700

    push to heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var path = require('path');
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
